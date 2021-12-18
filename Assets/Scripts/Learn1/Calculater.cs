d04a63881b15a5db6255e23c3490e7d2f4cb5c76
RockefellerArtem <pvapa15@mail.ru>
2021-12-18 19:49:27 +0300

    Learn 1 complet!

---
+++ Assets/Scripts/Learn1/Calculater.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Calculater : MonoBehaviour
+{
+    //Задача:
+    // Взять значение со скрипта TestName
+    // Затем нам нужно его изменить 
+    // и в конце снова вернуть его
+    //каждый этап выводить в консоль
+
+    private TestNamer _testNamer;
+    [SerializeField] private GameObject _contentTestNamer;
+
+    public void Start()
+    {
+        _testNamer = _contentTestNamer.GetComponent<TestNamer>();
+
+        int test = _testNamer.Count;
+        Debug.Log("Начальное число " + test);
+
+        _testNamer.Count = 9;
+        Debug.Log("Измененное значение " + _testNamer.Count);
+
+        _testNamer.Count = test;
+        Debug.Log("Опять начальное значение" + _testNamer.Count);
+    }
+}
---
+++ Assets/Scripts/Learn1/TestNamer.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System;
+
+public class TestNamer : MonoBehaviour
+{
+    private int _count = 5;
+    public int Count { get { return _count; } set {_count = value; } }
+
+
+}
