aecd286a4c7494831e2014d728844cc4249cfe1f
RockefellerArtem <pvapa15@mail.ru>
2021-12-20 19:05:03 +0300

    HW Learn 2

---
+++ Assets/Scripts/Learn2/HomeWork/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private Image _image;
+
+    [SerializeField] private Color[] _colors;
+
+    public void Red()
+    {
+        _image.color = _colors[0];
+    }
+
+    public void Yellow()
+    {
+        _image.color = _colors[2];
+    }
+
+    public void Green()
+    {
+        _image.color = _colors[1];
+    }
+}
